d1de5891b7abee753ee1e1bb523851aa4a8b9c4d
zephyr96 <aaadev32@gmail.com>
2023-02-02 13:16:57 -0800

    added more styling and images to HomePage component

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,15 +1,22 @@
-import NoMansSky from "../images/no-mans-sky"
+import NoMansSky from "../media/no-mans-sky.jpg"
+import QuakeLive from "../media/quake-live.jpg"
+import Titanfall from "../media/titanfall2.jpg"
+import Stalker from "../media/stalker-bundle.jpeg"
+import Metro from "../media/metro-bundle.avif"
+
 
 const HomeBody = () => {
+
     return (
         <div id="home-body">
             <ol id="home-event-items">
-                <li><a>placeholder item event sale <img></img></a></li>
-                <li><a>placeholder item event sale <img></img></a></li>
-                <li><a>placeholder item event sale <img></img></a></li>
+                <li><a href={'/shop'}><img src={NoMansSky}></img></a></li>
+                <li><a href={'/shop'}><img src={QuakeLive}></img></a></li>
+                <li><a href={'/shop'}><img src={Titanfall}></img></a></li>
             </ol>
             <ol id="home-bundle-items">
-                <li>placeholder bundle</li>
+                <li><a href={'/shop'}><img src={Stalker}></img></a></li>
+                <li><a href={'/shop'}><img src={Metro}></img></a></li>
             </ol>
             <div>
                 <h2 id="sale-event">generic jargon to head over to the shop and buy now!!!</h2>
@@ -18,4 +25,4 @@ const HomeBody = () => {
     );
 };
 
-export default HomeBody;
+export default HomeBody; 
