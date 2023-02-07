0db903ff4da63285a9f18e0b4f70b198f7079b80
zephyr96 <aaadev32@gmail.com>
2023-02-07 13:06:39 -0800

    added more styling to shop page, added button to cart, started filling placeholders with images,descriptions, and prices.

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,8 +1,8 @@
-import OpusMagnum from "../media/opus-magnum.jpg"
-import QuakeLive from "../media/quake-live.jpg"
-import Titanfall from "../media/titanfall2.jpg"
-import Stalker from "../media/stalker-bundle.jpeg"
-import Metro from "../media/metro-bundle.avif"
+import OpusMagnum from "../media/opus-magnum.jpg";
+import QuakeLive from "../media/quake-live.jpg";
+import Titanfall from "../media/titanfall2.jpg";
+import Stalker from "../media/stalker-bundle.jpeg";
+import Metro from "../media/metro-bundle.avif";
 
 
 const HomeBody = () => {
@@ -13,13 +13,13 @@ const HomeBody = () => {
                 <h3 id="home-sale-event">Celebrate Gamer Day (yes we made that up) with MASSIVE discounts up to 50% off!</h3>
             </div>
             <ol id="home-event-items">
-                <li><a href={'/shop'}><img src={OpusMagnum}></img></a></li>
-                <li><a href={'/shop'}><img src={QuakeLive}></img></a></li>
-                <li><a href={'/shop'}><img src={Titanfall}></img></a></li>
+                <li><a href={'/shop'}><img className="home-body-images" src={OpusMagnum} alt="game"></img></a></li>
+                <li><a href={'/shop'}><img className="home-body-images" src={QuakeLive} alt="game"></img></a></li>
+                <li><a href={'/shop'}><img className="home-body-images" src={Titanfall} alt="game"></img></a></li>
             </ol>
             <ol id="home-bundle-items">
-                <li><a href={'/shop'}><img src={Stalker}></img></a></li>
-                <li><a href={'/shop'}><img src={Metro}></img></a></li>
+                <li><a href={'/shop'}><img className="home-body-images" src={Stalker} alt="game"></img></a></li>
+                <li><a href={'/shop'}><img className="home-body-images" src={Metro} alt="game"></img></a></li>
             </ol>
 
         </div>
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,33 +1,93 @@
+import OpusMagnum from "../media/opus-magnum.jpg";
+import QuakeLive from "../media/quake-live.jpg";
+import Titanfall from "../media/titanfall2.jpg";
+import Stalker from "../media/stalker-bundle.jpeg";
+import Metro from "../media/metro-bundle.avif";
+import Factorio from "../media/factorio.avif";
+import EldenRing from "../media/elden-ring.jpg";
+import Doom from "../media/doom-eternal.jpg";
+import Aoe from "../media/aoe2.jpg";
+import HalfLife from "../media/half-life-2.jpg";
+import Deadspace from "../media/dead-space.jpg";
+import Bannerlord from "../media/bannerlord.jpg";
+
+
+
 
 const ShoppingCart = () => {
 
-    const addToCart = (itemKey) => {
-
-    }
 
     return (
         <div id="shopping-cart">
+
             <div id="cart">
                 <ol id="cart-items-list">
                     <li className="cart-items">place holder shop item image <div className="cart-item-price">placeholder price/disount</div></li>
-                    <li className="cart-items">place holder shop item image <div className="cart-item-price">placeholder price/disount</div></li>
-                    <li className="cart-items">place holder shop item image <div className="cart-item-price">placeholder price/disount</div></li>
+                    <button id="checkout-button">Checkout</button>
                 </ol>
             </div>
             <div id="shop">
                 <ol id="shop-items-list">
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
-                    <li className="shop-items">place holder shop item image <div className="shop-item-description"> placeholder description</div> <div className="shop-item-price">placeholder price/disount</div></li>
+                    <li className="shop-items">
+                        <img src={Aoe} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> Age of Empires II: Definitive Edition features “The Last Khans” with 3 new campaigns and 4 new Civilizations. Frequent updates include events, additional content, new game modes, and enhanced features with the recent addition of Co-Op mode! </div>
+                        <div className="shop-item-price">$14.99 from $19.99</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={Bannerlord} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description">A strategy/action RPG. Create a character, engage in diplomacy, craft, trade and conquer new lands in a vast medieval sandbox. Raise armies to lead into battle and command and fight alongside your troops in massive real-time battles using a deep but intuitive skill-based combat system. </div>
+                        <div className="shop-item-price">$29.99 from $49.99</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={Deadspace} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description">The sci-fi survival-horror classic returns, completely rebuilt to offer an even more immersive experience — including visual, audio, and gameplay improvements — while staying faithful to the original game’s thrilling vision. </div>
+                        <div className="shop-item-price">$59.99</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={Doom} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> Hell’s armies have invaded Earth. Become the Slayer in an epic single-player campaign to conquer demons across dimensions and stop the final destruction of humanity. The only thing they fear... is you. </div>
+                        <div className="shop-item-price">$19.99 from $39.99</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={EldenRing} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> THE NEW FANTASY ACTION RPG. Rise, Tarnished, and be guided by grace to brandish the power of the Elden Ring and become an Elden Lord in the Lands Between. </div>
+                        <div className="shop-item-price">$29.99 from $59.99</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={Factorio} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> placeholder description</div>
+                        <div className="shop-item-price">placeholder price/disount</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={HalfLife} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> placeholder description</div>
+                        <div className="shop-item-price">placeholder price/disount</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={Metro} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> placeholder description</div>
+                        <div className="shop-item-price">placeholder price/disount</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={OpusMagnum} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> placeholder description</div>
+                        <div className="shop-item-price">placeholder price/disount</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={QuakeLive} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> placeholder description</div>
+                        <div className="shop-item-price">placeholder price/disount</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={Stalker} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> placeholder description</div>
+                        <div className="shop-item-price">placeholder price/disount</div>
+                    </li>
+                    <li className="shop-items">
+                        <img src={Titanfall} className="shop-images" alt="game"></img>
+                        <div className="shop-item-description"> placeholder description</div>
+                        <div className="shop-item-price">placeholder price/disount</div>
+                    </li>
                 </ol>
             </div>
 
