aaf3d00ef2adc278136d0c6d44215b98b3a1ec5f
zephyr96 <aaadev32@gmail.com>
2023-03-03 11:32:40 -0800

    troubleshooting react app for github pages hosting

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route, Router, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Shop from "./pages/Shop";
 
 const RouteSwitch = () => {
     return (
-        <BrowserRouter>
-            < HashRouter basename="/">
+        <BrowserRouter basename="/">
+            <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/Shop" element={<Shop />} />
-            </HashRouter>
+            </Routes>
         </BrowserRouter>
     );
 };
